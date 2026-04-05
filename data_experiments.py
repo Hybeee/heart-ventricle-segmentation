0eb219efc5e9cb8c0927d0737a927deb76c37fda
Hybeee <szcsa2003@gmail.com>
2026-04-05 17:24:22 +0200

    TV denoising code added to data_experiments.py

--- data_experiments.py
+++ data_experiments.py
@@ -1,6 +1,7 @@
 import numpy as np
 import utils
 from scipy.ndimage import median_filter
+from skimage.restoration import denoise_tv_bregman, denoise_tv_chambolle
 import matplotlib.pyplot as plt
 
 import os
@@ -13,7 +14,7 @@ def _plot_result(title, ct, filtered_ct, cmap):
     filtered_ct_std = filtered_ct.std()
 
     title = (
-        f"{title}"
+        f"{title}\n"
         f"Standard Deviations: {ct_std:.4f} | {filtered_ct_std:.4f}"
     )
 
@@ -41,15 +42,30 @@ def _plot_median_filter_results(data_dir: str):
 
     _plot_result("Median Filter", ct, filtered_ct, "gray")
 
-def _plot_tv_results():
-    pass
+def _plot_tv_results(data_dir: str):
+    ct = np.load(os.path.join(data_dir, "ct.npy"))
+    ventricle_mask = np.load(os.path.join(data_dir, "ventricle.npy"))
+
+    ct_min = ct.min()
+    ct_max = ct.max()
+    ct = (ct - ct_min) / (ct_max - ct_min)
+
+    filtered_ct = denoise_tv_bregman(ct, weight=10.0)
+
+    ct = ct * (ct_max - ct_min) + ct_min
+    filtered_ct = filtered_ct * (ct_max - ct_min) + ct_min
+
+    ct[ventricle_mask == 0] = 0
+    filtered_ct[ventricle_mask == 0] = 0
+
+    _plot_result("TV Bregman", ct, filtered_ct, "gray")
 
 def main():
-    patient_id = "patient_0053"
+    patient_id = "patient_0002"
     data_dir = os.path.join("vis_output", patient_id, "preprocessing", "np")
 
-    _plot_median_filter_results(data_dir=data_dir)
-    _plot_tv_results()
+    # _plot_median_filter_results(data_dir=data_dir)
+    _plot_tv_results(data_dir=data_dir)
 
 if __name__ == "__main__":
     main()
