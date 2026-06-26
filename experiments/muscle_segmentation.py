62146fdd01eb4e86adf3e700b1a0e06d0909780c
Hybeee <szcsa2003@gmail.com>
2026-06-26 10:56:08 +0200

    Module import fix

--- experiments/muscle_segmentation.py
+++ experiments/muscle_segmentation.py
@@ -8,6 +8,11 @@ import SimpleITK as sitk
 import matplotlib.pyplot as plt
 
 import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 
 import utils
 
